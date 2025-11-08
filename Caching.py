58de7a32674ea30cfefac9f3b40453ce7b089982
Arijit Kayal <arijitkayal123@gmail.com>
2025-11-08 21:20:25 +0530

    Comments on caching
    # Take some time to execute
    # Takes almost negligible time to execute because of caching

--- Caching.py
+++ Caching.py
@@ -6,19 +6,22 @@ def fun(n):
     time.sleep(2)
     return "Number : "+str(n)
 
+# Take some time to execute
 print("Before caching")
 Start=time.time()
 print(fun(2))
 print(fun(5))
 print(fun(7))
 End=time.time()
-print("Total time before caching :",End-Start)
+print("Total time before caching :",End-Start) # More or less 6 sec 
 
+# Takes almost negligible time to execute because of caching
 print("\nAfter caching")
 Start=time.time()
 print(fun(2))
 print(fun(5))
 print(fun(7))
 End=time.time()
-print("Total time after caching :",End-Start)
+print("Total time after caching :",End-Start) # 0 sec
+
 
