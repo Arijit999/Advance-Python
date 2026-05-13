23f5a6e0f3320a8aeb8a29f569d288ae9251fe61
Arijit999 <arijitkayal123@gmail.com>
2026-05-13 18:41:48 +0530

    .
    Corrected terminology from 'deadlock' to 'race condition' in comments.

--- Lock&Key.py
+++ Lock&Key.py
@@ -2,9 +2,8 @@ import threading
 import time
 
 '''
-If lock is there then it will create 
-a deadlock condition as one thread increases 
-a value and other tryes to decrease.
+If lock is there then it will race condition as one 
+thread increases a value and other tryes to decrease.
 ''' 
 # Uncomment to enable lock
 shared_resource = 5
